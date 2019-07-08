c637867d35a9772bb085687d3ae55f5477d3b1d9
Hiran Silva <hiran.g.b.silva@gmail.com>
2019-07-08 16:25:25 -0300

    query user revisada

--- src/schema/queries/user.js
+++ src/schema/queries/user.js
@@ -1,31 +1,16 @@
 const { GraphQLNonNull, GraphQLID, GraphQLList } = require("graphql");
 const UserType = require("../types/user");
 
-const { User, UserWallet } = require("../../app/models");
+const { User } = require("../../app/models");
 
 const UserQueries = {
-  users: {
-    type: new GraphQLList(UserType),
-    description: "User",
-    resolve: async (_, __, ctx) => {
-      const value = await User.findAll();
-      console.log(value);
-      return value;
-    }
-  },
-  userById: {
+  user: {
     type: UserType,
     description: "User",
     args: {
       id: { type: new GraphQLNonNull(GraphQLID) }
     },
-    resolve: (_, { id }, ctx) => {
-      User.create({
-        name: "hiran",
-        email: "hiran@teste.com",
-        password: "12345678"
-      });
-    }
+    resolve: (_, { id }, ctx) => User.findByPk(id)
   }
 };
 
