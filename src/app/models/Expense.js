f392862db662a4080d36388f51f6c1d58dcb2b3d
Hiran Silva <hiran.g.b.silva@gmail.com>
2019-07-08 12:49:36 -0300

    model expense

---
+++ src/app/models/Expense.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+  const Expense = sequelize.define(
+    "Expense",
+    {
+      user_id: DataTypes.BIGINT,
+      type_id: DataTypes.BIGINT,
+      net_value: DataTypes.FLOAT,
+      plots: DataTypes.INTEGER,
+      plots_value: DataTypes.FLOAT,
+      initial_date: DataTypes.DATEONLY,
+      final_date: DataTypes.DATEONLY,
+      percentage: DataTypes.FLOAT,
+      description: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: "The description can not be empty"
+          },
+          len: {
+            args: [1, 100],
+            msg: "The description must contain less than 100 characters"
+          }
+        }
+      }
+    },
+    {
+      tableName: "expenses"
+    }
+  );
+
+  Expense.associate = models => {
+    Expense.belongsTo(models.User, { foreginKey: "user_id" });
+  };
+
+  Expense.associate = models => {
+    Expense.belongsTo(models.PaymentType, { foreginKey: "type_id" });
+  };
+
+  return Expense;
+};
