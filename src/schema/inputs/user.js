77b91cbb6d9e69e90a8c6e54f0ce09f9257a1fc3
Hiran Silva <hiran.g.b.silva@gmail.com>
2019-07-09 13:12:12 -0300

    add mutation de login

--- src/schema/inputs/user.js
+++ src/schema/inputs/user.js
@@ -4,10 +4,18 @@ const {
   GraphQLInputObjectType
 } = require("graphql");
 
-exports.Register = new GraphQLInputObjectType({
+exports.RegisterInput = new GraphQLInputObjectType({
   name: "Register",
   fields: {
     name: { type: new GraphQLNonNull(GraphQLString) },
+    email: { type: new GraphQLNonNull(GraphQLString) },
+    password: { type: new GraphQLNonNull(GraphQLString) }
+  }
+});
+
+exports.LoginInput = new GraphQLInputObjectType({
+  name: "LoginInput",
+  fields: {
     email: { type: new GraphQLNonNull(GraphQLString) },
     password: { type: new GraphQLNonNull(GraphQLString) }
   }
