c0bf8b06e578e0289e56ed3163dfe0c75c9fa2ee
Hiran Silva <hiran.g.b.silva@gmail.com>
2019-07-09 08:51:05 -0300

    add query wallet ao user

--- src/schema/queries/user.js
+++ src/schema/queries/user.js
@@ -1,16 +1,26 @@
 const { GraphQLNonNull, GraphQLID, GraphQLList } = require("graphql");
-const UserType = require("../types/user");
 
-const { User } = require("../../app/models");
+const UserType = require("../types/user");
+const UserWalletType = require("../types/wallet");
+
+const { User, UserWallet } = require("../../app/models");
 
 const UserQueries = {
   user: {
     type: UserType,
-    description: "User",
+    description: "Get User",
     args: {
       id: { type: new GraphQLNonNull(GraphQLID) }
     },
     resolve: (_, { id }, ctx) => User.findByPk(id)
+  },
+  wallet: {
+    type: UserWalletType,
+    description: "Get User Wallet",
+    args: {
+      id: { type: new GraphQLNonNull(GraphQLID) }
+    },
+    resolve: (_, { id }, ctx) => UserWallet.findByPk(id)
   }
 };
 
